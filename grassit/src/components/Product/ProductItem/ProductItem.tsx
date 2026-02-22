033aa9b56892c0f676d2a39a56de5a03d37b051c
jandutkiewicz0309 <jan.dutkiewicz@op.pl>
2026-02-22 14:28:16 +0100

    fix

--- grassit/src/components/Product/ProductItem/ProductItem.tsx
+++ grassit/src/components/Product/ProductItem/ProductItem.tsx
@@ -24,15 +24,11 @@ export const ProductItem: Component<IProductItem> = (props) => {
         <img class="product_img" src={props.img} alt={props.nameProduct} loading="lazy" />
       </div>
       <div class="text-container">
-        <div class={`name-price-info-container${isNaN(parseFloat(props.price)) ? " name-price-info-container--stacked" : ""}`}>
+        <div class="name-price-info-container">
           <span class="nameProduct">{props.nameProduct}</span>
           <span class={`price${isNaN(parseFloat(props.price)) ? " price--text" : ""}`}>{props.price}{!isNaN(parseFloat(props.price)) && <> zł <span class="price-netto">netto / m²</span></>}</span>
         </div>
         <span class="description">{props.description}</span>
-        <div class="bottom-info">
-          <FiDroplet size={11} class="icon" />
-          <span class="text">Komercyjne</span>
-        </div>
       </div>
     </div>
   );
